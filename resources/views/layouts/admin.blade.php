01a14e614391a52486dde4fad5ae63939dc590b9
sakeeo <ig.muhammadlim@gmail.com>
2022-09-06 22:54:33 +0800

    done - layanan rekomendasi

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -13,14 +13,14 @@
     <title>PELADEN</title>
 
     <!-- Fonts -->
-    <link href="{{ asset('public/vendor/fontawesome-free/css/all.min.css') }}" rel="stylesheet">
+    <link href="{{ asset('vendor/fontawesome-free/css/all.min.css') }}" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
 
     <!-- Styles -->
-    <link href="{{ asset('public/css/sb-admin-2.min.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/sb-admin-2.min.css') }}" rel="stylesheet">
 
     <!-- Favicon -->
-    <link href="{{ asset('public/img/favicon.png') }}" rel="icon" type="image/png">
+    <link href="{{ asset('img/favicon.png') }}" rel="icon" type="image/png">
     <link rel="stylesheet" href="//cdn.datatables.net/1.12.1/css/jquery.dataTables.min.css">
     @stack('css')
 </head>
@@ -228,10 +228,10 @@
 </div>
 
 <!-- Scripts -->
-<script src="{{ asset('public/vendor/jquery/jquery.min.js') }}"></script>
-<script src="{{ asset('public/vendor/bootstrap/js/bootstrap.min.js') }}"></script>
-<script src="{{ asset('public/vendor/jquery-easing/jquery.easing.min.js') }}"></script>
-<script src="{{ asset('public/js/sb-admin-2.min.js') }}"></script>
+<script src="{{ asset('vendor/jquery/jquery.min.js') }}"></script>
+<script src="{{ asset('vendor/bootstrap/js/bootstrap.min.js') }}"></script>
+<script src="{{ asset('vendor/jquery-easing/jquery.easing.min.js') }}"></script>
+<script src="{{ asset('js/sb-admin-2.min.js') }}"></script>
 
 <script src="//cdn.datatables.net/1.12.1/js/jquery.dataTables.min.js"></script>
 <script>
--- resources/views/layouts/auth.blade.php
+++ resources/views/layouts/auth.blade.php
@@ -11,22 +11,22 @@
     <title>{{ config('app.name', 'Laravel') }}</title>
 
     <!-- Fonts -->
-    <link href="{{ asset('public/vendor/fontawesome-free/css/all.min.css') }}" rel="stylesheet">
+    <link href="{{ asset('vendor/fontawesome-free/css/all.min.css') }}" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
 
     <!-- Styles -->
-    <link href="{{ asset('public/css/sb-admin-2.min.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/sb-admin-2.min.css') }}" rel="stylesheet">
 
     <!-- Favicon -->
-    <link href="{{ asset('public/img/favicon.png') }}" rel="icon" type="image/png">
+    <link href="{{ asset('img/favicon.png') }}" rel="icon" type="image/png">
 </head>
 <body class="bg-gradient-primary min-vh-100 d-flex justify-content-center align-items-center">
 
 @yield('main-content')
 
 <!-- Scripts -->
-<script src="{{ asset('public/vendor/jquery/jquery.min.js') }}"></script>
-<script src="{{ asset('public/vendor/bootstrap/js/bootstrap.min.js') }}"></script>
-<script src="{{ asset('public/js/sb-admin-2.min.js') }}"></script>
+<script src="{{ asset('vendor/jquery/jquery.min.js') }}"></script>
+<script src="{{ asset('vendor/bootstrap/js/bootstrap.min.js') }}"></script>
+<script src="{{ asset('js/sb-admin-2.min.js') }}"></script>
 </body>
 </html>
